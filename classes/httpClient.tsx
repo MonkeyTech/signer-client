b131c294923747ab07883bd39427c259a53cf989
OfirSalamon <ofir@monkeytech.co.il>
2022-05-24 09:39:38 +0300

    connect to the server

--- classes/httpClient.tsx
+++ classes/httpClient.tsx
@@ -4,13 +4,23 @@ class HttpClient {
   axios = axios.create({});
 
   constructor() {
-    this.axios.defaults.baseURL = "http://localhost:3000/api/v1";
+    this.axios.defaults.baseURL =
+      "https://madlanx-signer-server.herokuapp.com/api/v1";
   }
 
-  GetPDF = async () => {
-    const res = await this.axios.post(`/signature`);
-    return res;
+  GetPDF = async (token: string) => {
+    const res = await this.axios.get(`/signatures`,{
+      headers: {
+        'x-token': token,
+      },
+    });
+    return res.data.data;
   };
+
+  // presignedURL  = async () => {
+  //   const res = await this.axios.post(`/signatures/presigned_urls`)
+  //   return res.data.data;
+  // };
 }
 
 const httpClient = new HttpClient();
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,8 +5,8 @@ import PopUpModal from "../src/components/PopUpModal/PopUpModal";
 import { useState } from "react";
 import { Button } from "../src/components/Data/Signature.style";
 import { PDFDocument } from "pdf-lib";
-import download from "downloadjs";
 import { useEffect } from "react";
+import httpClient from "../classes/httpClient";
 interface Props {
   url: string;
 }
@@ -28,7 +28,8 @@ const Home = ({ url }: Props) => {
     width: 0,
   });
   const [recSize, setRecSize] = useState<IRecSize>({ x: 0, y: 0, width: 150 });
-
+  const [signedPDF, setSignedPDF] = useState();
+   
   async function getPDFPosition(url: string) {
     const pdfDocPdfBytes = await fetch(url).then((res) => res.arrayBuffer());
     const pdfDoc = await PDFDocument.load(pdfDocPdfBytes);
@@ -45,10 +46,6 @@ const Home = ({ url }: Props) => {
     });
   }
 
-  useEffect(() => {
-    getPDFPosition("/pdfs/output.pdf");
-  }, []);
-
   async function hadlePDFUplaod(imageURL: string | null) {
     if (!imageURL) return;
     const pdfDocPdfBytes = await fetch("/pdfs/output.pdf").then((res) =>
@@ -64,11 +61,16 @@ const Home = ({ url }: Props) => {
     const pngImage = await pdfDoc.embedPng(pngImageBytes);
     signatureFieldRect.setImage(pngImage);
     const pdfBytes = await pdfDoc.save();
+
   }
 
   useEffect(() => {
     hadlePDFUplaod(image);
   }, [image]);
+
+  useEffect(() => {
+    getPDFPosition("/pdfs/output.pdf");
+  }, []);
 
   return (
     <DocumentWrapper>
@@ -104,3 +106,20 @@ const Home = ({ url }: Props) => {
 };
 
 export default Home;
+
+export async function getStaticProps() {
+  try {
+    const { template_url } = await httpClient.GetPDF(
+      "da1652330dd51d4253231b79b59b05699da45ae07799822f6bd58b00c0f16f2201da78ac50bcf80ec7821b0566c1bd5b860dddde5f8a06ec73014a89284d416de57ec0f2b99768c4a51cb47e60e052bcdc281a851b2a23777adc1169aabe945b2efa2ef2f05cc1b512a050c110a3ae18bcc8b335714be3cf4109d165c2c7d9e9f94bf6c148c1382c5549a31537c08ec77f5463ee6428861637b7c266a4cd4044c13cb15cbc9ddbb61f0b5e0c2872db7116224632f4570aa99a86c09d9dcd2f6cdb230d8d5bfa636b485ec16e164fccfa2eb8f13c5154e217a689d6ca8057f16c7cece06a7f845863e3749ea7c24ab59a96a0d07623dbe5733850fe46fb596757"
+    );
+    return {
+      props: {
+        url: template_url,
+      },
+    };
+  } catch (err) {
+    console.log(err);
+
+    return { notFound: true };
+  }
+}
